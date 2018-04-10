e76eaacadca35c6f8857462c783b109f8db880d9
ljschaef <ljschaef@umich.edu>
2018-04-10 00:18:14 -0400

    Fuck.

--- final_proj_test.py
+++ final_proj_test.py
@@ -2,4 +2,9 @@
 # Final Project SI 206
 # Test File
 
+from final_proj import *
+import unittest
 
+class TestClassBuilder(unittest.TestCase):
+
+    # Now have function definitions in here of random tests
