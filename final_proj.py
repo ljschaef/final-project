2b9e67c0cfdcc0482f6902db9a24b7a01b4c535b
ljschaef <ljschaef@umich.edu>
2018-04-10 20:53:25 -0400

    Started the actual code

--- final_proj.py
+++ final_proj.py
@@ -5,11 +5,28 @@ import requests
 import sqlite3
 from bs4 import BeautifulSoup
 import plotly
+import json
 
 DBNAME = 'final_project.db'
-# INFO : Name, Fight Name, Feet, Inches, Weight, Wins, Losses, Draws, NCs, KOs,
-# Subs, Decisions
-CACHENAME = 'final_project.csv'
+# INFO : Name, Fight Name, Gender, Feet, Inches, Weight, Wins, Losses, Draws,
+# NCs, KOs, Subs, Decisions
+CACHELISTS = 'final_project.csv'
+name_list = []
+fightname_list = []
+gender_list = []
+feet_list = []
+inches_list = []
+weight_list = []
+wins_list = []
+losses_list = []
+draws_list = []
+ncs_list = []
+kos_list = []
+subs_list = []
+decisions_list = []
+# Cache of all the html shit, so you don't scape a page you already have
+CACHEPAGES = 'final_project.json'
+url_and_html_dict = {}
 
 # This is URL for 1st page of all fighters regardless of weight and gender
 # From here can crawl to fighter page, grab details, go to next one until done
@@ -31,8 +48,11 @@ def populate_db():
 
 def scrape_shit():
 
+    # This is also gonna do the caching I think
+    page_html = requests.get(baseurl).text
+    page_soup = BeautifulSoup(page_html, 'html.parser')
 
-
+    
 
     pass
 
