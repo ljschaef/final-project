8dc80b2acb8bf3aaf3c955d344285ce0c607df77
ljschaef <ljschaef@umich.edu>
2018-04-11 18:15:21 -0400

    Slow progress is still progress

--- final_proj.py
+++ final_proj.py
@@ -6,22 +6,12 @@ import sqlite3
 from bs4 import BeautifulSoup
 import plotly
 import json
+import csv
 
 DBNAME = 'final_project.db'
-# INFO : Name, Fight Name, Age, Height, Weight, Record, KOs, Subs, Decisions
+# INFO : Name, Fight Name, Age, Height, Weight, Record, Reach, Leg Reach
 CACHELISTS = 'final_project.csv'
-name_list = []
-fightname_list = []
-age_list = []
-height_list = []
-weight_list = []
-wins_list = []
-losses_list = []
-draws_list = []
-ncs_list = []
-kos_list = []
-subs_list = []
-decisions_list = []
+
 # Cache of all the html shit, so you don't scape a page you already have
 CACHEPAGES = 'final_project.json'
 url_and_html_dict = {}
@@ -30,23 +20,74 @@ url_and_html_dict = {}
 # From here can crawl to fighter page, grab details, go to next one until done
 # with page, then go to next page until all pages have been scraped
 baseurl = 'http://www.ufc.com/fighter/Weight_Class'
+truly_baseurl = 'http://www.ufc.com'
 
 def create_db():
 
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
 
+    statement = '''
+        DROP TABLE IF EXISTS 'Fighters';
+    '''
+
+    cur.execute(statement)
+
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE 'Fighters'(
+            'Name' TEXT PRIMARY KEY
+            'Fight Name' TEXT
+            'Age' INTEGER 
+            'Height' TEXT
+            'Weight' TEXT
+            'Record' TEXT
+            'Reach' TEXT
+            'Leg Reach' TEXT
+        );
+    '''
+
+    cur.execute(statement)
+
+    conn.commit()
+    conn.close()
 
     pass
 
 def populate_db():
 
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
 
+    with open(CACHELISTS) as cache:
+        fighter_info = csv.reader(cache)
+        row_num = 0
+        for row in fighter_info:
+            if row_num == 0:
+                row_num += 1
+            else:
+                insertion = (row[0], row[1], row[2], row[3], row[4], row[5],
+                             row[6], row[7])
+                statement = 'INSERT INTO "Fighters" '
+                statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+                cur.execute(statement, insertion)
 
 
     pass
 
 def scrape_shit():
 
+    name_list = []
+    fightname_list = []
+    age_list = []
+    height_list = []
+    weight_list = []
+    record_list = []
+    reach_list = []
+    legreach_list = []
     fighter_link_list = []
+    dicc = {}
     next_page_links_list = []
 
     # This is also gonna do the caching I think
@@ -55,36 +96,93 @@ def scrape_shit():
 
     # class="fighter-info" then find <a href>
 
+    # Need to do a thing here that goes to the next page and gets all the
+    # fighters there. Does this throughout all the pages. Then I can go ahead
+    # and do the rest of the stuff
+
     thing = page_soup.find_all(class_='fighter-info')
     new_thing = page_soup.find(class_='pagination')
     new_thing = new_thing.find('a')['href']
+    next_page_links_list.append(new_thing)
+    lit = '''
+    while new_thing is not None:
+        newy_url = truly_baseurl + str(new_thing)
+        bleck = requests.get(newy_url).text
+        more_bleh = BeautifulSoup(bleck, 'html.parser')
+        shit = more_bleh.find(class_='pagination')
+        tits = shit.find_all('li')
+        frack = shit
+    '''
+    newy_url = truly_baseurl + str(new_thing)
+    bleck = requests.get(newy_url).text
+    more_bleh = BeautifulSoup(bleck, 'html.parser')
+    shit = more_bleh.find(class_='pagination')
+    frack = shit
+    frack = frack.find_all('li')
+    for row in frack:
+        print(row)
+
+    # SERIOUSLY, LOOK AT THE ABOVE COMMENT LOL
 
     # thing = thing[0]
     # thing = thing.find('a')['href']
     # print(thing)
-
-    print(new_thing)
+    # print(new_thing)
 
     for fighter in thing:
         fighter_link = fighter.find('a')['href']
         fighter_link_list.append(fighter_link)
 
-
     for link in fighter_link_list:
-        new_url = baseurl + str(link)
-        new_html = requests.get(new_url)
+        new_url = truly_baseurl + str(link)
+        # print(new_url)
+        # fuck = '''
+        new_html = requests.get(new_url).text
         new_soup = BeautifulSoup(new_html, 'html.parser')
-        firstname = new_soup.find()
-        lastname = new_soup.find()
-        name = str(firstname) + ' ' + str(lastname)
+        possibly_name = new_soup.find(class_='floatl current')
+        if possibly_name is not None:
+            name = possibly_name.text
+            name_list.append(name)
+            # print(name)
+        else:
+            possibly_name = new_soup.find(class_='floatl current contender-series')
+            name = possibly_name.text
+            name_list.append(name)
+            # print(name)
+
         fightname = new_soup.find(id_='fighter-nickname')
-        age = new_soup.find()
-        height = new_soup.find()
-        weight = new_soup.find()
-        record = new_soup.find()
+        age = new_soup.find(id_='fighter-age')
+        height = new_soup.find(id_='fighter-height')
+        weight = new_soup.find(id_='fighter-weight')
+        reach = new_soup.find(id_='fighter-reach')
+        legreach = new_soup.find(id_='fighter-leg-reach')
+        record = new_soup.find(id_='fighter-skill-record')
+        fightname_list.append(fightname)
+        age_list.append(age)
+        height_list.append(height)
+        weight_list.append(weight)
+        reach_list.append(reach)
+        legreach_list.append(legreach)
+        record_list.append(record)
+        # '''
+    # print(len(name_list))
+    # print(len(fightname_list))
+    # print(len(age_list))
+    # print(len(height_list))
+    # print(len(weight_list))
+    # print(len(reach_list))
+    # print(len(legreach_list))
+    # print(len(record_list))
 
+    dicc['names'] = name_list
+    dicc['fightnames'] = fightname_list
+    dicc['ages'] = age_list
+    dicc['heights'] = height_list
+    dicc['weights'] = weight_list
+    dicc['reaches'] = reach_list
+    dicc['legreaches'] = legreach_list
+    dicc['recors'] = record_list
 
-
-    pass
+    return dicc
 
 scrape_shit()
